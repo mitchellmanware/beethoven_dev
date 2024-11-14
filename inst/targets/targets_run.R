7ff2caa8d7912f1899d5eb78bb69c8aa9099c0f2
mitchellmanware <memanware@gmail.com>
2024-11-14 17:28:29 -0500

    complex AQS listing

--- inst/targets/targets_run.R
+++ inst/targets/targets_run.R
@@ -3,12 +3,6 @@ target_run <-
     ############################################################################
     ############################################################################
     ###########################     CRITICAL TARGETS      ######################
-    # targets::tar_target(
-    #   chr_daterange,
-    #   command = c("2018-01-01", "2018-01-21"),
-    #   description = "Date range"
-    # )
-    # ,
     targets::tar_target(
       chr_daterange,
       command = amadeus::generate_date_sequence(
@@ -22,12 +16,6 @@ target_run <-
       description = "Input directory"
     )
     ,
-    # targets::tar_target(
-    #   num_dates_split,
-    #   command = 10,
-    #   description = "Number of days to include in each temporal split"
-    # )
-    # ,
     ############################################################################
     ############################################################################
     ############################################################################
@@ -40,60 +28,80 @@ target_run <-
       description = "Year range"
     )
     ,
-    # targets::tar_target(
-    #   list_dates,
-    #   command = beethoven::split_dates(
-    #     dates = chr_daterange,
-    #     n = num_dates_split,
-    #     year = TRUE,
-    #     julian = FALSE,
-    #     append = FALSE
-    #   ),
-    #   description = "Split date range into list"
-    # )
-    # ,
-    # targets::tar_target(
-    #   list_dates,
-    #   command = split(
-    #     chr_daterange,
-    #     ceiling(length(chr_daterange) / num_dates_split)
-    #   )
-    # )
-    # ,
-    # targets::tar_target(
-    #   chr_datenames,
-    #   command = names(list_dates),
-    #   description = "Names of date list"
-    # )
-    # ,
     ###########################         AQS          ###########################
     targets::tar_target(
-      sf_feat_proc_aqs_sites,
-      command = amadeus::process_aqs(
-        path = list.files(
-          path = file.path(
-            chr_input_dir,
-            "aqs",
-            "data_files"
-          ),
-          pattern = "daily_88101_[0-9]{4}.csv",
-          full.names = TRUE
-        ),
-        date = c(chr_daterange[1], chr_daterange[length(chr_daterange)]),
-        mode = "location",
-        return_format = "sf"
-      )[1:10, ],
+      list_feat_proc_aqs_sites,
+      command = {
+        if (!file.exists("/inst/extdata/list_feat_proc_aqs_sites.qs")) {
+          list_feat_proc_aqs_sites <- list(
+            amadeus::process_aqs(
+              path = list.files(
+                path = file.path(
+                  chr_input_dir,
+                  "aqs",
+                  "data_files"
+                ),
+                pattern = "daily_88101_[0-9]{4}.csv",
+                full.names = TRUE
+              ),
+              date = c(chr_daterange[1], chr_daterange[length(chr_daterange)]),
+              mode = "location",
+              return_format = "sf"
+            )
+          )
+          names(list_feat_proc_aqs_sites) <- seq(
+            1, length(list_feat_proc_aqs_sites)
+          )
+          qs::qsave(
+            list_feat_proc_aqs_sites,
+            "/inst/extdata/list_feat_proc_aqs_sites.qs"
+          )
+          qs::qsave(
+            chr_daterange[length(chr_daterange)],
+            "/inst/extdata/chr_dateend.qs"
+          )
+          list_feat_proc_aqs_sites
+        } else if (file.exists("/inst/extdata/list_feat_proc_aqs_sites.qs")) {
+          list_feat_proc_aqs_sites_prev <-
+            qs::qread("/inst/extdata/list_feat_proc_aqs_sites.qs")
+          chr_dateend <- qs::qread("/inst/extdata/chr_dateend.qs")
+          chr_datesnew <- chr_daterange[chr_daterange > chr_dateend]
+          list_feat_proc_aqs_sites_new <- list(
+            amadeus::process_aqs(
+              path = list.files(
+                path = file.path(
+                  chr_input_dir,
+                  "aqs",
+                  "data_files"
+                ),
+                pattern = "daily_88101_[0-9]{4}.csv",
+                full.names = TRUE
+              ),
+              date = c(chr_datesnew[1], chr_datesnew[length(chr_datesnew)]),
+              mode = "location",
+              return_format = "sf"
+            )
+          )
+          list_feat_proc_aqs_sites <- c(
+            list_feat_proc_aqs_sites_prev,
+            list_feat_proc_aqs_sites_new
+          )
+          names(list_feat_proc_aqs_sites) <- seq(
+            1, length(list_feat_proc_aqs_sites)
+          )
+          qs::qsave(
+            list_feat_proc_aqs_sites,
+            "/inst/extdata/list_feat_proc_aqs_sites.qs"
+          )
+          qs::qsave(
+            chr_datesnew[length(chr_datesnew)],
+            "/inst/extdata/chr_dateend.qs"
+          )
+          list_feat_proc_aqs_sites
+        }
+      },
       iteration = "list",
       description = "AQS sites"
-    )
-    ,
-    targets::tar_target(
-      list_feat_proc_aqs_sites,
-      command = split(
-        sf_feat_proc_aqs_sites,
-        sf_feat_proc_aqs_sites$site_id
-      ),
-      description = "Names of AQS sites list"
     )
     ,
     targets::tar_target(
--- run.sh
+++ run.sh
@@ -7,14 +7,14 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=50G
 #SBATCH --cpus-per-task=25
-#SBATCH --error=/ddn/gs1/home/manwareme/beethoven/dev/slurm/dev_%j.err
-#SBATCH --output=/ddn/gs1/home/manwareme/beethoven/dev/slurm/dev_%j.out
+#SBATCH --error=/ddn/gs1/home/manwareme/beethoven/beethoven_dev/slurm/dev_%j.err
+#SBATCH --output=/ddn/gs1/home/manwareme/beethoven/beethoven_dev/slurm/dev_%j.out
 
 # run pipeline in the container
 apptainer exec \
   --bind $PWD:/mnt \
   --bind $PWD/inst:/inst \
   --bind /ddn/gs1/group/set/Projects/NRT-AP-Model/input:/input \
-  --bind /ddn/gs1/home/manwareme/beethoven/dev/targets:/opt/_targets \
+  --bind /ddn/gs1/home/manwareme/beethoven/beethoven_dev/targets:/opt/_targets \
   beethoven_dl_calc.sif \
   Rscript --no-init-file /mnt/inst/targets/targets_start.R
