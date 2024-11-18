20fa6a57e552a43eeb743f53c1cce42e63f618f2
mitchellmanware <memanware@gmail.com>
2024-11-18 07:38:10 -0500

    clean break

--- inst/targets/targets_run.R
+++ inst/targets/targets_run.R
@@ -6,26 +6,8 @@ target_run <-
     targets::tar_target(
       chr_daterange,
       command = amadeus::generate_date_sequence(
-        "2018-01-01", "2018-01-10", sub_hyphen = FALSE
+        "2018-01-01", "2018-01-08", sub_hyphen = FALSE
       )
-    )
-    ,
-    targets::tar_target(
-      chr_dateend,
-      command = {
-        if (file.exists("/inst/extdata/chr_dateend.qs")) {
-          chr_dateend <- qs::qread("/inst/extdata/chr_dateend.qs")
-          chr_dateend
-        } else {
-          chr_dateend <- chr_daterange[1] - 1
-          chr_dateend
-        }
-      }
-    )
-    ,
-    targets::tar_target(
-      chr_daterun,
-      command = chr_daterange[chr_daterange > chr_dateend]
     )
     ,
     targets::tar_target(
@@ -38,82 +20,36 @@ target_run <-
     ############################################################################
     ###########################         AQS          ###########################
     targets::tar_target(
-      list_feat_proc_aqs_sites_prev,
-      command = {
-        if (file.exists("/inst/extdata/list_feat_proc_aqs_sites.qs")) {
-          list_feat_proc_aqs_sites_prev <- qs::qread(
-            "/inst/extdata/list_feat_proc_aqs_sites.qs"
-          )
-          list_feat_proc_aqs_sites_prev
-        } else {
-          list()
-        }
-      },
-      cue = targets::tar_cue("always"),
-      description = "AQS sites (previous)"
-    )
-    ,
-    targets::tar_target(
-      list_feat_proc_aqs_sites_new,
-      command = {
-        sf_feat_proc_aqs_sites_new <- amadeus::process_aqs(
-          path = list.files(
-            path = file.path(
-              chr_input_dir,
-              "aqs",
-              "data_files"
-            ),
-            pattern = "daily_88101_[0-9]{4}.csv",
-            full.names = TRUE
-          ),
-          date = c(chr_daterun[1], chr_daterun[length(chr_daterun)]),
-          mode = "location",
-          return_format = "sf"
-        )
-        chr_aqsprev <- as.vector(
-          unlist(
-            lapply(list_feat_proc_aqs_sites_prev, function(x) x$site_id)
-          )
-        )
-        chr_aqsnew <- setdiff(
-          sf_feat_proc_aqs_sites_new$site_id,
-          chr_aqsprev
-        )
-        sf_feat_proc_aqs_sites_filter <- sf_feat_proc_aqs_sites_new[
-          which(sf_feat_proc_aqs_sites_new$site_id %in% chr_aqsnew),
-        ]
-        list_feat_proc_aqs_sites_new <- list(sf_feat_proc_aqs_sites_filter)
-        names(list_feat_proc_aqs_sites_new) <-
-          length(list_feat_proc_aqs_sites_prev) + 1
-        list_feat_proc_aqs_sites_new
-      },
-      iteration = "list",
-      description = "AQS sites (new)"
-    )
-    ,
-    targets::tar_target(
       list_feat_proc_aqs_sites,
-      command = {
-        list_feat_proc_aqs_sites <- c(
-          list_feat_proc_aqs_sites_prev,
-          list_feat_proc_aqs_sites_new
-        )
-        qs::qsave(
-          list_feat_proc_aqs_sites,
-          "/inst/extdata/list_feat_proc_aqs_sites.qs"
-        )
-        qs::qsave(
-          chr_daterun[length(chr_daterun)],
-          "/inst/extdata/chr_dateend.qs"
-        )
-        list_feat_proc_aqs_sites
-      },
+      command = amadeus::process_aqs(
+        path = list.files(
+          path = file.path(
+            chr_input_dir,
+            "aqs",
+            "data_files"
+          ),
+          pattern = "daily_88101_[0-9]{4}.csv",
+          full.names = TRUE
+        ),
+        date = c(chr_daterange[1], chr_daterange[length(chr_daterange)]),
+        mode = "location",
+        return_format = "sf"
+      ) %>%
+        distinct(site_id, .keep_all = TRUE) %>%
+        split(.$site_id) %>%
+        lapply(function(sublist) {
+          sublist <- sublist %>%
+            sf::st_as_sf() %>% 
+            mutate(row_id = NULL)
+          rownames(sublist) <- NULL
+          sublist
+        }),
       description = "AQS sites"
     )
     ,
     targets::tar_target(
-      chr_aqsnames,
-      command = names(list_feat_proc_aqs_sites),
+      num_proc_aqs_sites,
+      command = seq(1, length(list_feat_proc_aqs_sites)),
     )
     ,
     ###########################       CALCULATE      ###########################
@@ -129,12 +65,11 @@ target_run <-
       command = par_narr(
         domain = chr_iter_calc_narr,
         path = file.path(chr_input_dir, "/narr/"),
-        date = c(chr_daterun, chr_daterun),
-        locs = list_feat_proc_aqs_sites[[chr_aqsnames]],
+        date = c(chr_daterange, chr_daterange),
+        locs = list_feat_proc_aqs_sites[[num_proc_aqs_sites]],
         nthreads = 1
       ),
-      pattern = cross(chr_iter_calc_narr, chr_daterun, chr_aqsnames),
-      cue = targets::tar_cue("thorough"),
+      pattern = cross(chr_daterange, num_proc_aqs_sites),
       iteration = "list",
       description = "Calculate NARR features (fit)"
     )
