1e5c9f1b74248e2917eb7519c7e3cba752ec90f8
kyle-messier <kyle.messier@nih.gov>
2024-11-18 13:37:40 -0500

    cross by process_aqs and process_narr only

--- inst/targets/targets_run.R
+++ inst/targets/targets_run.R
@@ -5,7 +5,7 @@ target_run <-
     ###########################     CRITICAL TARGETS      ######################
     targets::tar_target(
       chr_dates,
-      command = c("2018-01-01", "2018-01-15")
+      command = c("2018-12-20", "2019-01-03")
     )
     ,
 
@@ -36,49 +36,66 @@ target_run <-
 
   #############  DOWNLOAD #############
 
-  targets::tar_target(
-    name = download_aqs,
-    command = {
-      download_aqs(
-        parameter_code = "88101",
-        resolution_temporal = "daily",
-        year = chr_years,
-        directory_to_save = "/input/aqs/",
-        acknowledgement = TRUE,
-        download = TRUE,
-        remove_command = TRUE,
-        unzip = TRUE,
-        remove_zip = TRUE,
-        hash = TRUE
-      )
-    },
-    pattern = map(chr_years),
-    iteration = "list"
-  )
-  ,
+  # targets::tar_target(
+  #   name = download_aqs,
+  #   command = {
+      # amadeus::download_aqs(
+      #   parameter_code = "88101",
+      #   resolution_temporal = "daily",
+      #   year = 2019,
+      #   directory_to_save = "input/aqs/",
+      #   acknowledgement = TRUE,
+      #   download = TRUE,
+      #   remove_command = TRUE,
+      #   unzip = TRUE,
+      #   remove_zip = TRUE,
+      #   hash = TRUE
+      # )
+  #   },
+  #   pattern = map(chr_years),
+  #   iteration = "list"
+  # )
+  # ,
 
-      targets::tar_target(
-      download_narr,
-      command = amadeus::download_narr(
-        variables = chr_iter_calc_narr,
-        directory_to_save =  "/input/narr/",
-        year = chr_years,
-        remove_command = FALSE,
-        acknowledgement = TRUE,
-        download = TRUE,
-        hash = TRUE
-      ),
-      pattern = cross(chr_iter_calc_narr, chr_years),
-      iteration = "list"
-    )
-    ,
+    #   targets::tar_target(
+    #   download_narr,
+      # command = amadeus::download_narr(
+      #   variables = "air.sfc",
+      #   directory_to_save =  "input/narr/",
+      #   year = 2019,
+      #   remove_command = FALSE,
+      #   acknowledgement = TRUE,
+      #   download = TRUE,
+      #   hash = TRUE
+      # ),
+    #   pattern = cross(chr_iter_calc_narr, chr_years),
+    #   iteration = "list"
+    # )
+    # ,
+
+    # targets::tar_target(
+    #   name = download_narr_buffer,
+    #   command = {
+    #     download_narr
+    #     TRUE
+    #   }
+    # )
+    # ,
+
+    # targets::tar_target(
+    #   name = download_aqs_buffer,
+    #   command = {
+    #     download_aqs
+    #     TRUE
+    #   }
+    # )
+    # ,
 
   # ############### PROCESS ##################
 
   targets::tar_target(
     name = process_aqs,
     command = {
-      download_aqs
       process_aqs(
       path = "/input/aqs/data_files/",
       date = chr_daterange,
@@ -92,29 +109,36 @@ target_run <-
 
   ),
 
+geotargets::tar_terra_rast(
+    name = process_narr,
+    command = {
+      amadeus::process_narr(
+        path = paste0("/input/narr/", chr_iter_calc_narr[[1]]),
+        variable = chr_iter_calc_narr[[1]],
+        date = chr_daterange,
+      )
+    }, 
+    pattern = cross(chr_iter_calc_narr, chr_daterange)
+  )
+  ,
+
  
   ###########################       CALCULATE      ###########################
 
-    targets::tar_target(
+  targets::tar_target(
       calc_narr,
       command = {
-        process_aqs
-          narr <-   process_narr(
-          path = paste0("/input/narr/", chr_iter_calc_narr),
-            variable = chr_iter_calc_narr,
-            date = chr_daterange
-          )        
-        calculate_narr(
-          from = narr,
-          locs = process_aqs,
-          locs_id = "site_id",
-          radius = 0,
-          fun = "mean",
-          geom = "sf"
-        )
+          calculate_narr(
+              from = process_narr,
+              locs = process_aqs,
+              locs_id = "site_id",
+              radius = 0,
+              fun = "mean",
+              geom = "sf"
+          )
       },
-      pattern = cross(process_aqs, cross(chr_iter_calc_narr, chr_daterange)),
+      pattern = cross(process_aqs, process_narr),
       iteration = "list"
-    )
+  )
 
   )
